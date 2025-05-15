24662113c9097f720b65db958135e442142e82f6
Sahil Kamble <syk2448@gmail.com>
2025-05-15 17:31:17 +0530

    Initial project setup with Backend

---
+++ Backend/src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { createServer } from "node:http"; // it connects socket instance and express instance
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+const server = createServer(app); // that means..jab hum sevrer chalayenge tb app and io donon chalega server pr
+// const io = new Server(server);
+const io = connectToSocket(server);
+app.set("port", process.env.PORT || 8000);
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+app.get("/home", (req, res) => {
+  return res.json({ Hello: "world" });
+});
+
+app.use("/api/v1/users",userRoutes);
+
+const start = async () => {
+  const connectionDb = await mongoose.connect(
+    "mongodb+srv://syk2448:MyZoomApp@cluster0.ugscdkx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
+  server.listen(app.get("port"), () => {
+    console.log("Listening on port 8000");
+  });
+};
+
+start();
---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,82 @@
+import httpStatus from "http-status";
+import { User } from "../models/user.model.js";
+import bcrypt, { hash } from "bcrypt";
+import crypto from "crypto";
+// Login
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: `Please provide data` });
+  }
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res
+        .status(httpStatus.NOT_FOUND)
+        .json({ message: "User not found" });
+    }
+
+    // Compare the provided password with the stored hashed password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    // If the password is correct, generate a random token for the session
+    if (isPasswordValid) {
+      const token = crypto.randomBytes(20).toString("hex"); // Creates a random string of 20 bytes
+
+      // Assign the token to the user and save the updated user record
+      user.token = token;
+      await user.save();
+
+      // Respond with a 200 status and the token
+      return res.status(httpStatus.OK).json({ token: token });
+    } else {
+      // If the password is incorrect, respond with a 401 Unauthorized status
+      return res
+        .status(httpStatus.UNAUTHORIZED)
+        .json({ message: "Incorrect password" });
+    }
+  } catch (e) {
+    // If an error occurs, respond with a 500 status and the error message
+    return res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ message: `Something went wrong ${e}` });
+  }
+};
+
+const register = async (req, res) => {
+  const { name, username, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res
+        .status(httpStatus.FOUND)
+        .json({ message: "User already exists" }); // coming from http-status
+    }
+    // the password needs to be hashed before it is stored in the database.
+    // hashing is a one-way process (i.e., it cannot be reversed).
+    // it takes the password as an argument and returns a hashed version of it.
+    // We use 10 as the salt round (i.e., the number of times the password is hashed).
+    // A higher number makes the hash more secure, but it also makes it slower.
+    // The hashed password is then stored in the newUser object.
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name: name,
+      username: username,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(httpStatus.CREATED).json({ message: "User Registered" });
+  } catch (e) {
+    res.json({ message: `Something went wrong ${e}` });
+  }
+};
+
+// WHAT ARE TOKENS
+// In login authentication, a token is a secure and concise way to verify a user's identity and grant them access to a system or application without repeatedly requiring them to enter credentials. This token acts like an "e-key" or a "stamped ticket" that allows the user to enter and navigate within the application as long as the token remains valid.
+
+export { login, register };
